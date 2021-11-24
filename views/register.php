782fe4cc2c17d52604eae022669a26e7cd91dcd8
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-11-25 00:12:57 +0700

    update login and register

--- views/register.php
+++ views/register.php
@@ -1,43 +1,51 @@
-<h1>Đăng ký</h1><br>
-<div class="card-body">
-    <?php $form = app\core\Form\Form::begin('', "post") ?>
-        <div class="row">
-            <div class="col">
-                <?php echo $form->field($model, 'firstname') ?>
+<div class="card-regester">
+    <div class="card-header">
+        <h1>Đăng ký</h1>
+    </div>
+    <div class="card-body">
+        <?php $form = app\core\Form\Form::begin('', "post") ?>
+            <div class="row">
+                <div class="col">
+                    <?php echo $form->field($model, 'firstname') ?>
+                </div>
+                <div class="col">
+                    <?php echo $form->field($model, 'lastname') ?>
+                </div>
             </div>
-            <div class="col">
-                <?php echo $form->field($model, 'lastname') ?>
+            <div class="row">
+                <div class="col">
+                    <?php echo $form->field($model, 'email') ?>
+                </div>    
+                <div class="col">
+                    <?php echo $form->field($model, 'phone_number') ?>
+                </div>
+            </div> 
+            <div class="row">
+                <div class="col">
+                    <?php echo $form->field($model, 'address') ?>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <?php echo $form->field($model, 'email') ?>
-            </div>    
-            <div class="col">
-                <?php echo $form->field($model, 'phone_number') ?>
+            <div class="row">
+                <div class="col">
+                    <?php echo $form->field($model, 'password')->passwordField() ?>
+                </div>   
+                <div class="col">
+                    <?php echo $form->field($model, 'passwordConfirm')->passwordField() ?>
+                </div>  
             </div>
-        </div> 
-        <div class="row">
-            <div class="col">
-                <?php echo $form->field($model, 'address') ?>
+            <div class="remember-me">
+                <input type="checkbox">
+                <span style="color: #000000">I accept Terms of Service</span>
             </div>
+            <div class="btn">
+                <button type="submit" class="button">Đăng ký</button>
+            </div>
+        <?php app\core\form\Form::end() ?>
+    </div>
+    <div class="card-footer">
+        <div class="login">
+            Bạn đã có tài khoản chưa ?
+            <a href="/login"><button id="login-link">Đăng nhập</button></a>
         </div>
-        <div class="row">
-            <div class="col">
-                <?php echo $form->field($model, 'password')->passwordField() ?>
-            </div>   
-            <div class="col">
-                <?php echo $form->field($model, 'passwordConfirm')->passwordField() ?>
-            </div>  
-        </div>
-        <div class="remember-me">
-            <input type="checkbox">
-            <span style="color: #000000">I accept Terms of Service</span>
-        </div>
-        <button type="submit" class="button">Đăng ký</button>
-    <?php app\core\form\Form::end() ?>
-    <div class="login">
-        <p>Bạn đã có tài khoản chưa ?</p>
-        <a href="/login"><button id="login-link">Đăng nhập</button></a>
     </div>
 </div>
