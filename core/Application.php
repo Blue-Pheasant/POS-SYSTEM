544526e872fbb2322b12ecc81114daff9a7c000a
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2022-06-17 20:05:53 +0700

    Fix some bug

--- core/Application.php
+++ core/Application.php
@@ -60,7 +60,7 @@ class Application
 
     public static function isAdmin()
     {
-        return !self::$app->user->role === 'admin';
+        return self::$app->user->role == 'admin';
     }
 
     public function logout()
