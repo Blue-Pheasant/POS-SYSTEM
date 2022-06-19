d217e7f2f4c16519439dd1d9b526936aef0185da
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2022-06-19 22:46:20 +0700

    Change some method, constructor

--- core/Cookie.php
+++ core/Cookie.php
@@ -7,29 +7,25 @@ class Cookie
     private $name;
     private $value;
     private $time;
-    // private $path;
-    // private $domain;
     private $secure = false;
     
     public const KEY_COOKIE = "fa53b91ccc1b78668d5af58e1ed3a485";
     public const TIME_COOKIE = 3600 * 24 * 30;
 
-    public function __construct($name, $value, $time, $secure)
-    {
-        $this->name = $name;
-        $this->value = $value;
-        $this->time = $time;
-        $this->secure = $secure;
-    }
+    public function __construct() {}
 
     public static function exists($key)
     {
 		return (isset($_COOKIE[$key]));
 	}   
 
-    public function create() 
+    public function create($name, $value, $time, $secure) 
     {
-        return setcookie($this->name, $this->value, $this->time, $this->secure);
+        $this->name = $name;
+        $this->value = $value;
+        $this->time = $time;
+        $this->secure = $secure;
+        return setcookie($name, $value, $time, $secure);
     }
 
     public function destroy()
@@ -40,5 +36,25 @@ class Cookie
     public function get()
     {
         return $_COOKIE[$this->name];
+    }
+
+    public function getName() 
+    {
+        return $this->name;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function getSecure()
+    {
+        return $this->secure;
+    }
+
+    public function getTime()
+    {
+        return $this->time;
     }
 }
